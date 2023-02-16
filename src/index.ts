67b8f24027acf552f94bfccea25bc873e1c957ca
Sergi Casquero <sergicasquero@gmail.com>
2023-02-17 00:04:40 +0100

    Add user preferences questionaire

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import "./loadEnvironment.js";
 import express from "express";
 import morgan from "morgan";
 import thingsRouter from "./routers/thingsRouter.js";
+import { askCanEdit, askPort } from "./userSetup.js";
+
+let userPort: number;
+let canEdit: boolean;
+
+await askPort();
+await askCanEdit();
 
 const port = process.env.PORT ?? 4100;
 
