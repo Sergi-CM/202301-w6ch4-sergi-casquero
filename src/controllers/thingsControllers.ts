cfc89e411a87c7ba0ec664f3a2c30204432136a5
Sergi Casquero <sergicasquero@gmail.com>
2023-02-16 20:42:33 +0100

    Add router and controllers for endpoints with get method

---
+++ src/controllers/thingsControllers.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response } from "express";
+import thingsAlreadyKnow from "../data/thingsAlreadyKnow";
+
+export const getThingsList = (req: Request, res: Response) => {
+  res.status(200).json(thingsAlreadyKnow);
+};
+
+export const getThingById = (req: Request, res: Response) => {
+  const thingIknow = thingsAlreadyKnow.find(
+    (thing) => thing.id === +req.params.id
+  );
+  res.status(200).json(thingIknow);
+};
--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@ import "./loadEnvironment.js";
 import express from "express";
 import morgan from "morgan";
 import thingsAlreadyKnow from "./data/thingsAlreadyKnow.js";
+import {
+  getThingById,
+  getThingsList,
+} from "./controllers/thingsControllers.js";
+import { thingsRouter } from "./routers/thingsRouter.js";
 
 const port = process.env.PORT ?? 4100;
 
 const app = express();
 
-app.use(morgan("combined"));
+app.listen(port);
 
-app.get("/things", (req, res) => {
-  res.status(200).json(thingsAlreadyKnow);
-});
+app.use(morgan("dev"));
 
-app.get("/things/:id", (req, res) => {
-  const thingIknow = thingsAlreadyKnow.find(
-    (thing) => thing.id === +req.params.id
-  );
-  res.status(200).json(thingIknow);
-});
+app.use("/things", thingsRouter);
+
+app.get("/things", getThingsList);
+
+app.get("/things/:id", getThingById);
